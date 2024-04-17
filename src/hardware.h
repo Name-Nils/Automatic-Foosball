01650ed1f9b3d9f940c33b413e492e8b9c7f9370
Nils Schlein-Andersen <nils.schlein@gmail.com>
2024-04-17 14:21:00 +0200

    first draft

--- src/hardware.h
+++ src/hardware.h
@@ -8,21 +8,33 @@ namespace HARDWARE
     {
         // point couting and extra animations
         Adafruit_NeoPixel leds;
-        int score = 0;
 
-        const int led_count = 10;
+        int pin;
+
+        int led_count = 10;
 
     public:
+        
+        int score = 0;
 
         Point_light(int pin)
         {
-            this->leds = Adafruit_NeoPixel(led_count, pin, NEO_GRB + NEO_KHZ800);
+            this->pin = pin;
+            //this->setup();
+        }
+
+        Point_light() = default;
+
+        void setup()
+        {
+            this->leds = Adafruit_NeoPixel(led_count, this->pin);
             this->leds.begin();
             this->leds.setBrightness(255); // max brightness
             for (int i = 0; i < led_count; i++)
             {
                 this->leds.setPixelColor(i, 255,255,255); // set all leds to white
             }
+            this->leds.show();
         }
 
         void incriment()
@@ -32,6 +44,7 @@ namespace HARDWARE
                 this->leds.setPixelColor(this->score, 0, 255, 0); // green for received points
                 this->score ++;
             }
+            this->leds.show();
         }
 
         void decrement()
@@ -41,6 +54,7 @@ namespace HARDWARE
                 this->leds.setPixelColor(this->score, 255, 255, 255);
                 this->score --;
             }
+            this->leds.show();
         }
 
         void win_animation()
@@ -60,7 +74,7 @@ namespace HARDWARE
 
         void loop()
         {
-
+            //this->leds.show();
         }
     };
     class Motor
@@ -129,6 +143,12 @@ namespace HARDWARE
             this->target += distance;
         }
 
+        void stop()
+        {
+            // add the minimum distance for deceleration
+            this->position += pow(this->current_speed, 2) / (2 * this->acceleration) * ((this->current_speed > 0) ? 1 : -1);
+        }
+
         void loop()
         {
             // make sure that motor never accelerates over the allowed amount and that the motor reaches target position
@@ -152,33 +172,39 @@ namespace HARDWARE
     };
     class Button
     {
-        // removal of noise from button press
-        // function pointer that points to functionality on button press (not release)
-        void (*function)();
+    protected:
+
         int pin;
         bool last_state = false;
         unsigned long int last_change = 0;
-        const unsigned long int min_time_change = 50; // button must be depressed for at least 50 milliseconds and likewise when released
+        unsigned long int min_time_change = 50; // button must be depressed for at least 50 milliseconds and likewise when released
         bool actuated = true;
 
     public:
 
-        Button(int pin, void (*function)())
+        bool button_state = false;
+
+        Button(int pin)
         {
             this->pin = pin;
             pinMode(this->pin, INPUT);
-            this->function = function;
         }
 
-        void loop(bool actuate_on_press) // alternatively on release
+        Button() = default;
+
+        bool loop(bool actuate_on_press) // alternatively on release
         {
-            bool button_state = digitalRead(this->pin);
-            if (button_state == this->last_state)
+            bool rtn = false;
+            this->button_state = digitalRead(this->pin);
+            if (this->button_state == this->last_state)
             {
                 if (millis() - this->last_change > this->min_time_change)
                 {
+                    if (this->button_state == actuate_on_press && !this->actuated) 
+                    {
+                        rtn = true;
+                    }
                     this->actuated = true;
-                    if (button_state == actuate_on_press) this->function();
                 }
             }
             else
@@ -190,11 +216,150 @@ namespace HARDWARE
                 }
                 this->last_change = millis();
             }
+            this->last_state = this->button_state;
+            return rtn;
+        }
+    };
+    class Button_func: protected Button
+    {
+        // removal of noise from button press
+        // function pointer that points to functionality on button press (not release)
+        void (*function)();
+        
+    public:
+
+        Button_func(int pin, void (*function)())
+        {
+            this->pin = pin;
+            pinMode(this->pin, INPUT);
+            this->function = function;
+        }
+
+        Button_func(int pin)
+        {
+            this->pin = pin;
+            pinMode(this->pin, INPUT);
+        }
+
+        Button_func() = default;
+
+        void loop(bool actuate_on_press)
+        {
+            if (this->Button::loop(actuate_on_press))
+            {
+                this->function();
+            }
         }
     };
     class Control_buttons
     {
         // use the button class as either child for this class or as internal object and then layer with lighting elements
+        Button start;
+        Button mode;
+        Button reset;
+        Adafruit_NeoPixel leds;
+
+        enum PIN
+        {
+            RESET=0,
+            MODE=1,
+            START=2
+        };
+
+    public:
+        
+        enum STATE
+        {
+            STANDBY,
+            GAME,
+            OVER
+        };
+
+        STATE state;
+
+        Control_buttons(int led, int start, int mode, int reset)
+        {
+            this->start = Button(start);  
+            this->mode = Button(mode);
+            this->reset = Button(reset);
+            this->leds = Adafruit_NeoPixel(3, led);
+            this->leds.begin();
+
+            this->state = this->STANDBY;
+
+            this->leds.setPixelColor(this->START, 0,255,0);
+            this->leds.setPixelColor(this->MODE, 255,255,255);
+            this->leds.setPixelColor(this->RESET, 0,0,0);
+
+            this->leds.show();
+        }
+
+        void start_loop()
+        {
+            if (this->start.loop(true))
+            {
+                // start button clicked
+                this->state = this->GAME;
+                this->color_update();
+            }
+        }
+
+        void reset_loop()
+        {
+            if (this->reset.loop(true))
+            {
+                this->state = this->STANDBY;
+                this->color_update();
+            }
+        }
+
+        void color_update()
+        {
+            uint8_t r,g,b;
+            r = g = b = 0;
+
+            switch (this->state)
+            {
+            case this->STANDBY:
+                g = 255;
+                break;
+            case this->GAME:
+                r = 255;
+                g = 165;
+                break;
+            case this->OVER:
+                //black
+                break;
+            default:
+                break;
+            }
+            this->leds.setPixelColor(this->START, r,g,b);
+
+            r = g = b = 0;  
+            switch (this->state)
+            {
+            case this->STANDBY:
+                // black
+                break;
+            case this->GAME:
+                r = 200;
+                break;
+            case this->OVER:
+                r = 255;
+                break;
+            default:
+                break;
+            }
+            this->leds.setPixelColor(this->OVER, r,g,b);
+
+            this->leds.show();
+        }
+
+        void loop()
+        {
+            this->start_loop();
+            this->reset_loop();
+        }
     };
     class Lighting
     {
--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,48 @@
 #include <Adafruit_NeoPixel.h>
 
 
-HARDWARE::Motor motor(A1, A0, A2);
-HARDWARE::Point_light p1_light(A3);
+HARDWARE::Control_buttons control(1,1,1,1);
 
-void point_incriment()
+enum P
 {
-    p1_light.incriment();
-    motor.advance(200*10);
-}
+    P1,
+    P2 
+};
+HARDWARE::Point_light point_led[2] = {HARDWARE::Point_light(5), HARDWARE::Point_light(1)};
+HARDWARE::Motor motor(A1, A0, A2);
 
-HARDWARE::Button p1_button(A5, point_incriment);
+namespace BUTTON_FUNCS
+{
+    void ball_lift()
+    {
+        motor.stop();
+        motor.disable(); // motor should not be active continously
+    }
+    void update_points(int player)
+    {
+        if (control.state == control.GAME)
+        {
+            point_led[player].incriment();
+            if (point_led[player].score >= 10)
+            {
+                control.state = control.OVER;
+                control.color_update();
+                return;
+            }
+        }
+        motor.advance(200*40*10); // around to 10 rotations of the motor
+    }
+    void p1()
+    {
+        update_points(P1);
+    }
+    void p2()
+    {
+        update_points(P2);
+    }
+} // namespace BUTTON_FUNCS
+HARDWARE::Button_func point_button[2] = {HARDWARE::Button_func(1, BUTTON_FUNCS::p1), HARDWARE::Button_func(1, BUTTON_FUNCS::p2)};
+HARDWARE::Button_func ball_button(1, BUTTON_FUNCS::ball_lift);
 
 
 void setup()
@@ -24,5 +56,7 @@ void setup()
 void loop()
 {
     motor.loop();
-    p1_button.loop(true);
+    ball_button.loop(true);
+    for (int i = 0; i < 2; i ++) { point_button[i].loop(true); }
+    control.loop();
 }
